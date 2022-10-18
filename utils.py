99439892f2ad310e5b6eaa0a55262217d45e94d3
mariano <mcabezas@eia.udg.edu>
2022-10-18 13:39:57 +1100

    improved the registration

--- utils.py
+++ utils.py
@@ -1,34 +1,179 @@
+import cv2
 import torch
 import numpy as np
+from gimpformats.gimpXcfDocument import GimpDocument
 from scipy.ndimage import binary_erosion, binary_dilation
 from skimage.morphology import skeletonize
 import torch.nn.functional as func
+from matplotlib.image import imread
 
 
-def warp_ellipse(image, theta, height, width, a, b, center_x, center_y):
+"""
+Image reading functions
+"""
+
+
+def load_atlas_sample(path):
+    """
+    This function is created to interact with US images from Girona. As such,
+    we need to remove certain UX data.
+
+    :param path: Path to the image.
+    :return: data containing a numpy array ready for a network and image with
+     the raw imaging data.
+    """
+
+    image = clean_ux(imread(path).copy()[..., 0])
+    data = normalise_image(image)
+
+    return data, image
+
+
+def load_xcf(path):
+    """
+    Given the name of a xcf file, this functions reads it using GimpDocument,
+    extract a list of images along with a list of layer names and returns them
+    as two lists.
+    :param path: Path to the image.
+    :return: list of the layer names and their data (images).
+    """
+
+    # List data on groups followed by the direct children of a gimp xcf
+    # document.
+    project = GimpDocument(path)
+    layers = project.layers
+    # CAUTION: We are including the image. It's important if we only want the
+    # coordinates of the annotatons, as we will need to ignore it!
+    names, data = zip(*[
+        (layer.name, layer.image) for layer in layers
+        if not layer.isGroup
+    ])
+    labels = list(names)[:-1]
+    annotations = list(data)[:-1]
+    points = [get_points(np.array(x)) for x in annotations]
+    image = clean_ux(np.array(data[-1])[..., 0])
+    data = normalise_image(image)
+
+    return image, data, labels, points
+
+
+def normalise_image(image):
+    """
+    Function to normalise the image to its intensity z-scores. No region
+    of interest is assumed.
+    :param image: Ultrasound image
+    :return: a normalised array ready for training
+    """
+    # Data normalisation and preparation for the network(s).
+    image_norm = (image - image.mean()) / image.std()
+    data = np.repeat(
+        np.expand_dims(image_norm, 0), 3, axis=0
+    ).astype(np.float32)
+
+    return data
+
+
+def clean_ux(image):
+    """
+    Function designed to clean the UX from a US image from Girons.
+
+    :param image: Image with the UX.
+    :return: a clean image without UX.
+    """
+    new_image = image.copy()
+    new_image[:50, ...] = 0
+    new_image[:150, 700:] = 0
+    new_image[:, :40] = 0
+
+    return new_image
+
+
+def get_points(image):
+    """
+    Function designed to take an image with manually drawn points and return
+    the set of coordinates for their centroid.
+
+    :param image: RGB image with the point annotations.
+    :return:  numpy aray with the centroids (points x coordinates).
+    """
+    # We us OpenCV to binarize the image and detect the points.
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray[gray > 0] = 255
+
+    # We use OpenCV to compute the centroids of the annotated points.
+    _, _, _, centroids = cv2.connectedComponentsWithStats(gray)
+
+    # We ignore the background centroid (first and largest one).
+    return centroids[1:]
+
+
+"""
+Ellipse functions
+"""
+
+
+def warp_image_ellipse(image, theta, height, width, a, b, center_x, center_y):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    theta = torch.tensor(theta, device=device, requires_grad=True)
     half_x = width / 2
     half_y = height / 2
-    offset_x = center_x - half_x
-    offset_y = center_y - half_y
+    theta = torch.tensor(theta, device=device, requires_grad=True)
     x = torch.arange(start=0, end=width, step=1)
     y = torch.arange(start=0, end=height, step=1)
     grid_x, grid_y = torch.meshgrid(x, y, indexing='xy')
-    grid_x_c = (grid_x - center_x).to(device)
-    grid_y_c = (grid_y - center_y).to(device)
-    new_x = grid_x_c * torch.cos(theta) - grid_y_c * torch.sin(theta) + offset_x
-    new_y = grid_y_c * torch.cos(theta) + grid_x_c * torch.sin(theta) + offset_y
+    new_x, new_y = rotate_points_origin(
+        grid_x, grid_y, theta, center_x, center_y
+    )
+    offset_x = center_x - half_x
+    offset_y = center_y - half_y
+    ratio_x = a / half_x
+    ratio_y = b / half_y
 
-    image_tensor = torch.from_numpy(image).float().view(1, 1, height, width).to(device)
+    rot_offset_x, rot_offset_y = rotate_points(
+        torch.tensor(offset_x), torch.tensor(offset_y), theta
+    )
+
+    offset_x += ratio_x * rot_offset_x
+    offset_y += ratio_y * rot_offset_y
+
+    image_tensor = torch.from_numpy(image).float().view(1, 1, height, width)
     grid_tensor = torch.stack([
-        new_x.float().to(device) * (2 * a) / (half_x ** 2),
-        new_y.float().to(device) * (2 * b) / (half_y ** 2)
+        2 * (ratio_x * new_x.float().to(device) + offset_x) / width,
+        2 * (ratio_y * new_y.float().to(device) + offset_y) / height
     ], dim=-1).unsqueeze(0)
 
-    registered_image = func.grid_sample(image_tensor, grid_tensor)
+    registered_image = func.grid_sample(
+        image_tensor.to(grid_tensor.device), grid_tensor
+    )
 
-    return registered_image.view(height, width).cpu().detach().numpy()
+    return registered_image.view(height, width)
+
+
+def rotate_points(x, y, theta):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    gpu_x = x.to(device)
+    gpu_y = y.to(device)
+
+    warped_x = gpu_x * torch.cos(theta) - gpu_y * torch.sin(theta)
+    warped_y = gpu_y * torch.cos(theta) + gpu_x * torch.sin(theta)
+
+    return warped_x, warped_y
+
+
+def rotate_points_origin(x, y, theta, center_x, center_y):
+    x_c = (x - center_x)
+    y_c = (y - center_y)
+
+    warped_x, warped_y = rotate_points(x_c, y_c, theta)
+
+    return warped_x, warped_y
+
+
+def warp_points_ellipse(x, y, theta, center_x, center_y, half_x, half_y, a, b):
+    ratio_x = half_x / a
+    ratio_y = half_y / b
+    rot_x, rot_y = rotate_points_origin(x, y, theta, center_x, center_y)
+
+    return ratio_x * (rot_x + a), ratio_y * (rot_y + b)
 
 
 def robust_fit_ellipse(mask, n_iter=100, multiloop=5, lr=1):
